d33dc2bb5cfa03181de7942e89289945feffc00f
Al Beltran <lamnethuskar4@gmail.com>
2023-03-07 11:46:46 +0800

    Added assesment commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Dashboard } from './pages/dashboard/dashboard';
-import { customTheme } from './theme/customTheme';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,4 +1,6 @@
 import React, { FC, ReactElement } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { customTheme } from '../../theme/customTheme';
 
 import { Grid } from '@mui/material';
 import { Sidebar } from '../../components/sidebar/sidebar';
@@ -6,9 +8,12 @@ import { TaskArea } from '../../components/taskArea/taskArea';
 
 export const Dashboard: FC = (): ReactElement => {
   return (
-    <Grid container minHeight="100vh" p={0} m={0}>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Grid container minHeight="100vh" p={0} m={0}>
       <TaskArea />
       <Sidebar />
-    </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
